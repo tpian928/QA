008a195118d2acaf81ca9a02781a6e62472a859c
tpian928 <tpian928@icloud.com>
2015-12-26 14:07:57 +0800

    回答问题

--- WebContent/js/question_detail.js
+++ WebContent/js/question_detail.js
@@ -111,6 +111,37 @@ $(document).on('click', ".disagreeBtn", function() {
     }
 });
 
+$('#answerBtn').on('click', function () {
+    o("answerBtn");
+
+    var answer_content = $('#answer_text').val();
+    var uid = localStorage.uid;
+
+    if (isEmpty(answer_content)==false&&isEmpty(uid)==false) {
+
+        var uid = localStorage.uid;
+		o("q_id here is "+q_id);
+
+    	answer(answer_content,uid,q_id,function (data) {
+
+    		var result = parseInt(data.result)
+    		if (result==0) {
+    			window.location.href="/QA/index.html";
+    		}
+    		else{
+    			alert("错误");
+    		}
+
+    	});
+
+    }
+    else{
+    	alert("错误的填写");
+    }
+
+
+
+});
 
 
 
